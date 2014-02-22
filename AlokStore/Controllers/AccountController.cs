feb5849130983e2dede5de07976ff968e64b2491
joydeba <joydeba@yahoo.com>
2014-02-22 23:28:43 +0600

    First on Remote

--- AlokStore/Controllers/AccountController.cs
+++ AlokStore/Controllers/AccountController.cs
@@ -5,8 +5,7 @@ using System.Transactions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
+
 using WebMatrix.WebData;
 using AlokStore.Filters;
 using AlokStore.Models;

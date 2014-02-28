17808c64d977b40a7ca836344618d598135a453a
joydeba <joydeba@yahoo.com>
2014-02-28 22:59:05 +0600

    first success host copy

--- AlokStore/Models/AccountModels.cs
+++ AlokStore/Models/AccountModels.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Globalization;
 using System.Web.Security;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlokStore.Models
 {
